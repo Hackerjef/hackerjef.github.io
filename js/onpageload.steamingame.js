e2cced8e8ae7dbf409e554a02d9840cce4bda45c
august <auguststenz@aol.com>
2016-08-20 18:29:54 -0400

    Fixed alot of shit
    Signed-off-by: august <auguststenz@aol.com>

---
+++ js/onpageload.steamingame.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+  document.getElementById('google').value='';
+  document.getElementById('steamrep').value='';
+  document.getElementById('nhsbans').value='';
+  document.getElementById('nhscomms').value='';
+  document.getElementById('steam').value='';
+  document.getElementById('youtube').value='';
+}
--- js/textboxstuff.js
+++ js/textboxstuff.js
@@ -1,6 +1,89 @@
-function inputFocus(i){
-    if(i.value==i.defaultValue){ i.value=""; i.style.color="#000"; }
+// Released under MIT license: http://www.opensource.org/licenses/mit-license.php
+
+$('[placeholder]').focus(function() {
+  var input = $(this);
+  if (input.val() == input.attr('placeholder')) {
+    input.val('');
+    input.removeClass('placeholder');
+  }
+}).blur(function() {
+  var input = $(this);
+  if (input.val() == '' || input.val() == input.attr('placeholder')) {
+    input.addClass('placeholder');
+    input.val(input.attr('placeholder'));
+  }
+}).blur().parents('form').submit(function() {
+  $(this).find('[placeholder]').each(function() {
+    var input = $(this);
+    if (input.val() == input.attr('placeholder')) {
+      input.val('');
+    }
+  })
+});
+// searchthings
+function googlesearchKeyPress(e)
+{
+    // look for window.event in case event isn't passed in
+    e = e || window.event;
+    if (e.keyCode == 13)
+    {
+        document.getElementById('googlebutton').click();
+        return false;
+    }
+    return true;
 }
-function inputBlur(i){
-    if(i.value==""){ i.value=i.defaultValue; i.style.color="#888"; }
+function steamrepsearchKeyPress(e)
+{
+    // look for window.event in case event isn't passed in
+    e = e || window.event;
+    if (e.keyCode == 13)
+    {
+        document.getElementById('steamrepbutton').click();
+        return false;
+    }
+    return true;
+}
+function nhsbanssearchKeyPress(e)
+{
+    // look for window.event in case event isn't passed in
+    e = e || window.event;
+    if (e.keyCode == 13)
+    {
+        document.getElementById('nhsbansbutton').click();
+        return false;
+    }
+    return true;
+}
+function nhscommssearchKeyPress(e)
+{
+    // look for window.event in case event isn't passed in
+    e = e || window.event;
+    if (e.keyCode == 13)
+    {
+        document.getElementById('nhscommsbutton').click();
+        return false;
+    }
+    return true;
+}
+function steamsearchKeyPress(e)
+{
+    // look for window.event in case event isn't passed in
+    e = e || window.event;
+    if (e.keyCode == 13)
+    {
+        document.getElementById('steambutton').click();
+        return false;
+    }
+    return true;
+}
+function youtubesearchKeyPress(e)
+{
+    // look for window.event in case event isn't passed in
+    e = e || window.event;
+    if (e.keyCode == 13)
+    {
+        document.getElementById('youtubebutton').click();
+        return false;
+    }
+    return true;
 }
